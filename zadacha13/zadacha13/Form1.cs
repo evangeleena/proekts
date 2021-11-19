59d64c4e71624c3a70561786eab65d8553531373
evangeleena <94674431+evangeleena@users.noreply.github.com>
2021-11-19 11:32:28 +0300

    Add files via upload

---
+++ zadacha13/zadacha13/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zadacha13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string textSum = textBox1.Text;
+            int p = Convert.ToInt32(textSum);
+
+            string textMes = textBox2.Text;
+            int BB = Convert.ToInt32(textMes);
+            int BBB = BB / 12;
+            int t = BBB + 1; 
+
+            string textProc = textBox3.Text;
+            int n = Convert.ToInt32(textProc);
+
+            int Sum = p + (p * t * n) / 100;
+            textBox4.Text = Convert.ToString(Sum);
+
+        }
+    }
+}
---
+++ zadacha14/zadacha14/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zadacha14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int cena = 0;
+            int n;
+            int sum;
+            if (radioButton1.Checked)
+            { cena = 100; }
+            if (radioButton2.Checked)
+            { cena = 50; }
+            n = Convert.ToInt32(textBox1.Text);
+            sum = cena * n;
+            textBox2.Text = Convert.ToString(sum);
+        }
+    }
+}
